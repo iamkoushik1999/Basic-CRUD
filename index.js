ea8383883f696f45ff862d6fed069a2b52e464d4
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-02-20 21:14:07 +0530

    index.js updated

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const textRoutes = require("./routes/textRoutes");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 9001;
@@ -7,7 +11,7 @@ const PORT = process.env.PORT || 9001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use("/api/texts", textRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}!`);
