9b6b607b0d9416087e542f0a674409c1768bd050
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-02-21 22:18:24 +0530

    added timestamps

--- models/textModel.js
+++ models/textModel.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
-const textSchema = mongoose.Schema({
-  text: {
-    type: String,
-    required: [true, "please add some text"],
+const textSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, "please add some text"],
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Text", textSchema);
